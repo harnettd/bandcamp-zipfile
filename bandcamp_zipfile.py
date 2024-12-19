c3f51255bfdedf14fdd34e57314f56a65078c5e4
Derek Harnett <DHarnett.dev@proton.me>
2024-12-19 09:40:20 -0800

    refactor

--- bandcamp_zipfile.py
+++ bandcamp_zipfile.py
@@ -1,73 +1,109 @@
-'''Unzip zip files from Bandcamp.'''
+"""
+Unzip Bandcamp zip files.
+
+Unzip Bandcamp zip files to an artist/album directory structure. Replace
+whitespace with underscores in all path names.
+
+@author: Derek Harnett
+"""
 from pathlib import Path
 import re
 from zipfile import ZipFile
 
 class BandcampZipFile(ZipFile):
-    def __init__(self, file, *args, **kwargs):
-        super().__init__(file, *args, **kwargs)
+    def __init__(self, file, **kwargs) -> None:
+        """
+        Create a new BandcampZipFile instance.
 
-        self.path = Path(self.filename)
-        artist_album_info = self.path.stem.split(' - ') 
+        :param file: path of a Bandcamp zip file
+        :type file: str or pathlike object
+        """
+        super().__init__(file, **kwargs)
+
+        self.stem: str = Path(self.filename).stem
+        artist_album_info: list[str] = self.stem.split(' - ') 
         if len(artist_album_info) == 2:
-            self.artist = artist_album_info[0].title()
-            self.album = artist_album_info[1].title()
+            self.artist: str = artist_album_info[0].title()
+            self.album: str = artist_album_info[1].title()
         else:
-            raise ValueError(f'Bad filename, {self.filename}')
+            raise ValueError(f'Bad Bandcamp zipfile name, {self.filename}')
 
     @staticmethod
-    def remove_whitespace(s: str):
-        "Replace whitespace with underscores."
+    def remove_whitespace(s: str) -> str:
+        """
+        Trim whitespace and replace remaining whitespace with underscores.
+
+        Usage:
+        >>> BandcampZipFile.remove_whitespace('  a  b ')
+        'a_b'
+        """
         return re.sub(r'\s+', '_', s.strip())
     
     @staticmethod
-    def starts_with_two_digits(name):
-        return re.match(r'\d\d', name)
+    def starts_with_two_digits(s: str) -> bool:
+        """
+        Return True if s starts with two digits, False otherwise.
+
+        Usage:
+        >>> BandcampZipFile.starts_with_two_digits('05 Song Title')
+        True
+        >>> BandcampZipFile.starts_with_two_digits('5 Song Title')
+        False
+        """
+        return re.match(r'\d\d', s) is not None
     
-    def make_new_name(self, member_name):
-        new_name = member_name.removeprefix(self.path.stem + ' - ')
-        new_name = Path(new_name)
-        new_name = new_name.stem.title() + new_name.suffix
+    def make_new_name(self, name: str) -> str:
+        """
+        Make a new name for a member of the zipfile.
+        """
+        new_name = Path(name)
+        stem: str = new_name.stem
+        suffix: str = new_name.suffix
+        new_name: str = stem.removeprefix(self.stem + ' - ').title() + suffix
         new_name = BandcampZipFile.remove_whitespace(new_name)
 
-        # If the first two characters of new_name are digits, 
-        # then insert '_-' between the digits and the rest of
-        # new_name.
         if BandcampZipFile.starts_with_two_digits(new_name):
             new_name = new_name[:2] + '_-' + new_name[2:]  
         
         return new_name
         
-    def extractall(self, dest):
-        # assume that dest is a Path
-        artist_dir = BandcampZipFile.remove_whitespace(self.artist.title())
-        album_dir = BandcampZipFile.remove_whitespace(self.album.title())
-        artist_album_path = dest / artist_dir / album_dir
-        print(artist_album_path)
+    def extractall(self, dest) -> None:
+        """
+        Extract all members of the zipfile.
+
+        :param dest: Directory to unzip to
+        :type dest: string or pathlike
+        """
+        if isinstance(dest, str):
+            dest = Path(dest)
+
+        # Make a directory for the album, if one doesn't already exist.
+        artist_dir: str = BandcampZipFile.remove_whitespace(self.artist.title())
+        album_dir: str = BandcampZipFile.remove_whitespace(self.album.title())
+        artist_album_path: Path = dest / artist_dir / album_dir
         try:
             artist_album_path.mkdir(parents=True)
         except FileExistsError:
+            # If the directory already exists, then do nothing.
             pass
+
+        # For each member of the zipfile, extract and rename.
+        member_name: str
         for member_name in self.namelist():
-            new_member_name = self.make_new_name(member_name)
-            member = self.extract(member_name, path=artist_album_path)
-            new_member = artist_album_path / new_member_name
-            print(f'Name: {member_name} of type {type(member_name)}')
-            print(f'New Name: {new_member_name} of type {type(new_member_name)}')
-            print(f'Path: {member} of type {type(member)}')
-            print(f'New Path: {new_member} of type {type(new_member)}')
-            print()
+            new_member_name: str = self.make_new_name(member_name)
+            member: str = self.extract(member_name, path=artist_album_path)
+            new_member: Path = artist_album_path / new_member_name
             Path(member).rename(new_member)
 
     def __str__(self):
-        # return f'Path: {self.path}'
-        return f'Filename: {self.path.name}\n' +\
+        return f'Filename: {self.filename}\n' +\
+            f'Stem: {self.stem}\n' +\
             f'Artist: {self.artist}\n' +\
             f'Album: {self.album}'
 
 if __name__ == '__main__':
-    # print(__doc__)
+    print(__doc__)
 
     bczf = BandcampZipFile('zip/Riot City - Burn The Night.zip')
-    # print(bczf)
+    print(bczf)
     bczf.extractall(Path('music'))
